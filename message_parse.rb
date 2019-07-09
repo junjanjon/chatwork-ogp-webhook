9814102904f3db6d10adcea0ba9160ae26e64e67
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-07-09 15:08:49 +0900

    first commit

---
+++ message_parse.rb
@@ -0,0 +1,78 @@
+require 'faraday'
+require 'open_graph_reader'
+require 'uri'
+
+def get_url_expander(url)
+	url_expander = %x(curl -I -s "#{url}" | grep -i Location | cut -d ' ' -f 2)
+	return url if url_expander.length == 0
+	url_expander.chomp
+end
+
+def ogp_parse(url)
+	p url = get_url_expander(url)
+	p object = OpenGraphReader.fetch(url)
+
+	return {
+		title: 'OpenGraphReader 失敗',
+		description: '',
+	} if object.nil?
+
+	p data = {
+		title: object.og.title,
+		image_url: object.og.image.url,
+		description: object.og.description,
+	}
+
+	return data
+end
+
+def get_url(message)
+	match_result = message.match(/(http[^ \s\r\n\[]*)/)
+	return nil if match_result.nil?
+	match_result[1]
+end
+
+def download_image_file(image_url)
+	response = Faraday.get(image_url)
+	filename = "OGP_" + image_url.split('/').last.chomp(':large')
+	File.write(filename, response.body)
+	return filename
+end
+
+def parse(message)
+	p message
+
+	# p match_result = message.match(/ogp/)
+	# return nil if match_result.nil?
+	
+	url = get_url(message)
+	return nil if url.nil?
+	ogp_data = ogp_parse(url)
+	return nil if ogp_data.nil?
+
+	unless ogp_data[:image_url].nil?
+		filename = download_image_file(ogp_data[:image_url])
+		ogp_data[:filename] = filename
+	end
+
+	return ogp_data
+rescue => error
+	p "失敗したにゃ #{error}"
+	return nil
+end
+
+
+
+if __FILE__ == $0
+	parse('https://sai-zen-sen.jp/works/extras/sfa026/01/01.html ogp')
+	# parse('http://www.kenoh.com/2019/06/25_emaki.html?fbclid=IwAR0-8-Acz5x9tEtnj-gxNa_50YLWyxc9T_N5rC7rUXhPi-ou-tBatvHbg3U
+ # ogp')
+
+	# parse('https://ift.tt/2X2Khle ogp')
+	# parse('ogp https://www.bbc.com/japanese/48767764')
+	# parse('http://ogp.me について教えて')
+	# parse('https://ref.xaio.jp/ruby/classes/string/split について教えて ogp')
+	# parse('https://mail.google.com/mail/u/0/#inbox')
+	# parse('https://gamebiz.jp/?p=241852 ogp')
+	# parse('https://qiita.com/akicho8/items/efe59578f12d6b7f5626 ogp')
+end
---
+++ server.rb
@@ -0,0 +1,56 @@
+require 'json'
+require 'webrick'
+require_relative './message_parse'
+require 'openssl'
+require 'base64'
+require 'chatwork_webhook_verify'
+
+CHATWORK_TOKEN = ENV.fetch('CHATWORK_TOKEN')
+WEBHOOK_TOKEN = ENV.fetch('WEBHOOK_TOKEN')
+
+s = WEBrick::HTTPServer.new(
+  :Port => 18080,
+  :HTTPVersion => WEBrick::HTTPVersion.new('1.1'),
+  :AccessLog => [[open(IO::NULL, 'w'), '']] # アクセスログを出力しない
+)
+
+def handling_request(req)
+  puts req.raw_header
+  return unless req.body
+  puts verify = ChatworkWebhookVerify.verify?(token: WEBHOOK_TOKEN, body: req.body, signature: req['X-ChatWorkWebhookSignature'])
+  return unless verify
+  p body = JSON.parse(req.body)
+  return if body['webhook_event_type'] != 'mention_to_me'
+  room_id = body['webhook_event']['room_id']
+  message = body['webhook_event']['body']
+
+  ogp_data = parse(message)
+  return if ogp_data.nil?
+  p ogp_data
+  puts response = "#{ogp_data[:title]}[hr]#{ogp_data[:description]}"
+
+  token = CHATWORK_TOKEN
+  if ogp_data[:filename].nil?
+  	%x(curl -v -X POST -H "X-ChatWorkToken: #{token}" -d 'body=#{response}&self_unread=1' "https://api.chatwork.com/v2/rooms/#{room_id}/messages")
+  else
+    filepath = ogp_data[:filename]
+    %x(curl -v -X POST -H "X-ChatWorkToken: #{token}" -F"file=@#{filepath}" -F'message=#{response}' "https://api.chatwork.com/v2/rooms/#{room_id}/files")
+  end
+rescue => e
+  STDERR.puts e.backtrace.join("\n")
+  STDERR.puts 'なにか問題が発生しました。'
+end
+
+
+s.mount_proc('/') do |req, res|
+  puts "========== #{Time.new} =========="
+  # レスポンス内容を出力
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = 'OK'
+
+  # リクエスト内容を出力
+  handling_request(req)
+end
+
+s.start
