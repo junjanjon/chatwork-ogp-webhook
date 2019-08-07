f99076c515179203e41bacf0b25c8fe429752242
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-08-07 17:08:04 +0900

    small fix

--- message_parse.rb
+++ message_parse.rb
@@ -18,9 +18,9 @@ def ogp_parse(url)
 	} if object.nil?
 
 	p data = {
-		title: object.og.title,
+		title: object.og.title.gsub(/'/, ''),
 		image_url: object.og.image.url,
-		description: object.og.description,
+		description: object.og.description.gsub(/'/, ''),
 	}
 
 	return data
@@ -34,7 +34,7 @@ end
 
 def download_image_file(image_url)
 	response = Faraday.get(image_url)
-	filename = "OGP_" + image_url.split('/').last.chomp(':large')
+	filename = "OGP_" + image_url.split('/').last.chomp(':large').split('?')[0]
 	File.write(filename, response.body)
 	return filename
 end
@@ -64,7 +64,10 @@ end
 
 
 if __FILE__ == $0
-	parse('https://sai-zen-sen.jp/works/extras/sfa026/01/01.html ogp')
+	# p parse("https://supersalariedman.blogspot.com/2018/11/blog-post_74.html")
+	# p parse('https://twitter.com/LoveLive_staff/status/1156374027180658690')
+	p parse('https://www.thanko.jp/shopdetail/000000003314/')
+	p parse('https://note.mu/rangatarou/n/n6541adc4c855')
 	# parse('http://www.kenoh.com/2019/06/25_emaki.html?fbclid=IwAR0-8-Acz5x9tEtnj-gxNa_50YLWyxc9T_N5rC7rUXhPi-ou-tBatvHbg3U
  # ogp')
 
