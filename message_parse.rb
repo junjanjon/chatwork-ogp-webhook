d9cb84e326fe45b967d67f22ea7b656c390c27fe
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-08-17 18:20:11 +0900

    open graph parse 修正

--- message_parse.rb
+++ message_parse.rb
@@ -10,23 +10,24 @@ def getOpenGraph(url)
     charset = f.charset # 文字種別を取得
     f.read # htmlを読み込んで変数htmlに渡す
   end
-  doc = Nokogiri::HTML.parse(html)
+
+  doc = Nokogiri::HTML.parse(html, charset)
 
   data = {}
 
   site_name_content = doc.css('//meta[property="og:site_name"]/@content')
 
-  data[:title] = if site_name_content.nil?
+  data[:title] = if site_name_content.empty?
                    doc.title.to_s
                  else
-                   doc.css('//meta[property="og:site_name"]/@content').to_s
+                   site_name_content.to_s
                  end
 
   description_content = doc.css('//meta[property="og:description"]/@content')
-  data[:description] = if description_content.nil?
+  data[:description] = if description_content.empty?
                          doc.css('//meta[name$="escription"]/@content').to_s
                        else
-                         doc.css('//meta[property="og:description"]/@content').to_s
+                         description_content.to_s
                        end
 
   data[:image_url] = doc.css('//meta[property="og:image"]/@content').to_s
@@ -60,7 +61,7 @@ def ogp_parse(url)
     description: data[:description].gsub(/'/, '')
   }
 
-  data
+  p data
 end
 
 def get_url(message)
@@ -86,7 +87,7 @@ def parse(message)
   ogp_data = ogp_parse(url)
   return nil if ogp_data.nil?
 
-  unless ogp_data[:image_url].nil?
+  unless ogp_data[:image_url].nil? or ogp_data[:image_url].empty?
     filename = download_image_file(ogp_data[:image_url])
     ogp_data[:filename] = filename
   end
@@ -103,9 +104,11 @@ if $PROGRAM_NAME == __FILE__
   # p parse('https://www.thanko.jp/shopdetail/000000003314/')
   # p parse('https://note.mu/rangatarou/n/n6541adc4c855').nil?
   # p parse('https://ift.tt/2X2Khle ogp').nil?
-  # p parse('ogp https://www.bbc.com/japanese/48767764')
+  # p parse('ogp https://www.bbc.com/japanese/48767764').nil?
   # p parse('http://ogp.me について教えて').nil?
-  p parse('https://ref.xaio.jp/ruby/classes/string/split について教えて ogp').nil?
+  # p parse('https://ref.xaio.jp/ruby/classes/string/split について教えて ogp').nil?
+  # p parse('https://www.toyomaru.jp/main/machine/sushizanmai_gokujyou/').nil?
+
   # p parse('https://gamebiz.jp/?p=241852 ogp').nil?
   # p parse('https://qiita.com/akicho8/items/efe59578f12d6b7f5626 ogp').nil?
 end
