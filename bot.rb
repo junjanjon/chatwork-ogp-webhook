9a77bf0beff7fd0e5299a07f32f593dc314e2180
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-08-18 16:21:58 +0900

    add all_room_bot

--- bot.rb
+++ bot.rb
@@ -1,32 +1,58 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'chatwork'
 require_relative './message_parse'
 
-WATCH_ROOM_ID = ENV.fetch('WATCH_ROOM_ID')
 CHATWORK_TOKEN = ENV.fetch('CHATWORK_TOKEN')
+ChatWork.api_key = CHATWORK_TOKEN
 
-WATCH_ROOM_ID.split(',').each do |room_id|
-  messages = `curl -X GET -H "X-ChatWorkToken: #{CHATWORK_TOKEN}" "https://api.chatwork.com/v2/rooms/#{room_id}/messages?force=0"`
-
-  next if messages.empty?
-
-  p messages = JSON.parse(messages)
-
-  messages.each do |data|
-    ogp_data = parse(data['body'])
-    next if ogp_data.nil?
-
-    p ogp_data
-    response = "#{ogp_data[:title]}[hr]#{ogp_data[:description]}"
-
-    if ogp_data[:filename].nil?
-      `curl -v -X POST -H "X-ChatWorkToken: #{CHATWORK_TOKEN}" -d 'body=#{response}&self_unread=1' "https://api.chatwork.com/v2/rooms/#{room_id}/messages"`
-    else
-      filepath = ogp_data[:filename]
-      `curl -v -X POST -H "X-ChatWorkToken: #{CHATWORK_TOKEN}" -F"file=@#{filepath}" -F'message=#{response}' "https://api.chatwork.com/v2/rooms/#{room_id}/files"`
-    end
-    sleep(1)
-  end
-  sleep(1)
+def get_unread_rooms
+  all_rooms = ChatWork::Room.get
+  all_rooms.reject { |room| room.unread_num.zero? }
 end
+
+def get_me_account_id
+  me = ChatWork::Me.get
+  me.account_id
+end
+
+def read_room(room_id:)
+  messages = ChatWork::Message.get(room_id: room_id, force: true)
+  ChatWork::Message.read(room_id: room_id, message_id: messages.last.message_id)
+rescue StandardError => e
+  p "失敗したにゃ #{e} #{e.backtrace}"
+end
+
+def main
+  p me_account_id = get_me_account_id
+  p unread_rooms = get_unread_rooms
+
+  unread_rooms.each do |room|
+    messages = ChatWork::Message.get(room_id: room.room_id)
+
+    if messages.nil?
+      read_room(room_id: room.room_id)
+      next
+    end
+
+    messages.reject { |message| message.account.account_id == me_account_id }.each do |data|
+      ogp_data = parse(data.body)
+      next if ogp_data.nil?
+
+      response = "#{ogp_data[:title]}[hr]#{ogp_data[:description]}"
+
+      if ogp_data[:filename].nil?
+        ChatWork::Message.create(room_id: room.room_id, body: response)
+      else
+        filepath = ogp_data[:filename]
+        file = Faraday::UploadIO.new(filepath, 'multipart/mixed')
+        ChatWork::File.create(room_id: room.room_id, file: file, message: response)
+      end
+    end
+
+    ChatWork::Message.read(room_id: room.room_id, message_id: messages.last.message_id)
+  end
+end
+
+main
